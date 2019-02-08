d8bfd3d836a404a490cdef2d687431e8c9c86e8f
ejacques <eytanjacques@gmail.com>
2019-02-08 03:30:45 +0200

    FFFUUUUCCCCKKKK

---
+++ .emacs.d/init.el
@@ -0,0 +1,43 @@
+
+
+
+;; setup package manager
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+
+;; Bootstrap 'use-package' - This package helps in installing packages
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+
+
+
+
+
+
+
+		      
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet-snippets flycheck which-key use-package try org-link-minor-mode org-bullets auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
